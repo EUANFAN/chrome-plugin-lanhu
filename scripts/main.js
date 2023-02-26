c89a7dbc931c0c1e8e8d1a32c4621ae1a6fe2d3d
范园贺 <w_fanyuanhe@xiwang.com>
2023-02-26 09:23:32 +0800

    feat: 主题文件修改

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,13 @@
 window.onload = function () {
+  /**
+   * todo:
+   * 1、THEME_CSS_MAP读取线上文件，避免更新两份
+   * 2、支持外部配置，修改主题形式
+   */
   const THEME_CSS_MAP = {
     /* 字体 */
     "--dandelion-font-family":
-      "pingfang sc, -apple-system, helvetica neue, helvetica, arial, hiragino sans gb, source han sans, noto sans cjk sc, microsoft yahei, sans-serif",
+      "pingfang sc, -apple-system, helvetica neue, helvetica,arial, hiragino sans gb, source han sans, noto sans cjk sc, microsoft yahei,sans-serif",
 
     /* 特殊标题 */
     "--dandelion-font-size-display-1": "28px",
@@ -25,83 +30,94 @@ window.onload = function () {
     "--dandelion-font-weight-2": "400",
 
     /* 主色 */
-    "--dandelion-color-primary": "#FF3627",
-    "--dandelion-background-color-primary": "#FF3627",
+    "--dandelion-color-primary": "#ff3627",
+    "--dandelion-background-primary": "#ff3627",
 
     /* 主色-渐变 */
     "--dandelion-color-primary-gradient":
-      "linear-gradient(315deg,#ff3627 0%, #ff664f 100%)",
+      "linear-gradient(315deg, #ff3627 0%, #ff664f 100%)",
+    "--dandelion-background-primary-gradient":
+      "linear-gradient(315deg, #ff3627 0%, #ff664f 100%)",
 
     /* 辅助色 */
-    "--dandelion-color-secondary": "#E3A96D",
+    "--dandelion-color-secondary": "#e3a96d",
+    "--dandelion-background-secondary": "#e3a96d",
 
     /* 通知-底色 */
-    "--dandelion-color-notice-background": "#E0EAF8",
+    "--dandelion-background-color-notice": "#e0eaf8",
 
     /* 通知-文本色 */
-    "--dandelion-color-notice-text": "#0662DF",
+    "--dandelion-color-notice-text": "#0662df",
 
     /* 提示-底色 */
-    "--dandelion-color-tips-background": "#FFF5E8",
+    "--dandelion-background-color-tips": "#fff5e8",
 
     /* 提示-文本色 */
-    "--dandelion-color-tips-text": "#FF9D00",
+    "--dandelion-color-tips-text": "#ff9d00",
 
     /* 警告-底色 */
-    "--dandelion-color-warning-background": "#FDE6E4",
+    "--dandelion-background-color-warning": "#fde6e4",
 
     /* 警告文本色 */
-    "--dandelion-color-warning-text": "#F44522",
+    "--dandelion-color-warning-text": "#f44522",
 
     /* 中性色 */
     "--dandelion-color-grey-1": "#333333",
     "--dandelion-color-grey-2": "#666666",
     "--dandelion-color-grey-3": "#999999",
-    "--dandelion-color-grey-4": "#C1C1C1",
-    "--dandelion-color-grey-5": "#F6F6F6",
-    "--dandelion-color-grey-6": "#F3F3F4",
-    "--dandelion-color-grey-7": "#F7F7F8",
-    "--dandelion-color-grey-8": "#FFFFFF",
+    "--dandelion-color-grey-4": "#c1c1c1",
+    "--dandelion-color-grey-5": "#d6d6d6",
+    "--dandelion-color-grey-6": "#f3f3f4",
+    "--dandelion-color-grey-7": "#f7f7f8",
+    "--dandelion-color-grey-8": "#ffffff",
 
     /* 中性色-背景 */
     "--dandelion-background-grey-1": "#333333",
     "--dandelion-background-grey-2": "#666666",
     "--dandelion-background-grey-3": "#999999",
-    "--dandelion-background-grey-4": "#C1C1C1",
-    "--dandelion-background-grey-5": "#F6F6F6",
-    "--dandelion-background-grey-6": "#F3F3F4",
-    "--dandelion-background-grey-7": "#F7F7F8",
-    "--dandelion-background-grey-8": "#FFFFFF",
+    "--dandelion-background-grey-4": "#c1c1c1",
+    "--dandelion-background-grey-5": "#d6d6d6",
+    "--dandelion-background-grey-6": "#f3f3f4",
+    "--dandelion-background-grey-7": "#f7f7f8",
+    "--dandelion-background-grey-8": "#ffffff",
 
     /* 圆角 */
     "--dandelion-border-radius-1": "16px",
     "--dandelion-border-radius-2": "12px",
     "--dandelion-border-radius-3": "8px",
     "--dandelion-border-radius-4": "2px",
+
+    /* 阴影 */
+    "--dandelion-box-shadow-1": "0px 2px 10px 0px rgba(0,0,0,0.08)",
+    "--dandelion-box-shadow-2": "0px 0px 10px 0px rgba(0,0,0,0.08);",
   };
 
   const FILTER_FILEDS = ["font-family"];
 
-  class ChromePluginLanhuapp {
+  class ChromePluginLanhu {
     constructor() {
       this.instance = null;
       this.codeBoxInnerHtml = null;
       this.codeDetailDom = null;
       this.subtitleDom = null;
-      this.styleStr =
+      this.transBtnStyleStr =
         "padding: 0 19px; display: inline-block; border-radius: 16px; background: #2878ff; cursor: pointer; color: #fff; line-height: 32px; position: relative; top: 4px; font-weight: 500; float: right; margin-right: 40px;";
+      this.resetBtnStyleStr =
+        "padding: 0 19px; display: inline-block; border-radius: 16px; background: rgba(40, 120, 255, 0.15); cursor: pointer; color: #2878ff; line-height: 32px; position: relative; top: 4px; font-weight: 500; float: right; margin-right: 40px;";
     }
 
     static getInstance = () => {
       if (!this.instance) {
-        this.instance = new ChromePluginLanhuapp();
+        this.instance = new ChromePluginLanhu();
       }
       return this.instance;
     };
 
     createNode = (eleType, eleText) => {
       const ele = document.createElement(eleType);
-      ele.innerHTML = eleText;
+      if (eleText) {
+        ele.innerHTML = eleText;
+      }
       return ele;
     };
 
@@ -124,22 +140,26 @@ window.onload = function () {
       codeCssList.forEach((cssItem) => {
         if (cssItem) {
           for (let themeItem in THEME_CSS_MAP) {
-            const trimCssKey = cssItem.key.trim();
-            const trimCssVal = cssItem.value.trim();
-            const trimThemeKey = themeItem.trim();
-            if (FILTER_FILEDS.includes(trimCssKey)) {
+            if (FILTER_FILEDS.includes(cssItem.key)) {
               cssItem.value = `var(${themeItem})`;
               return;
             }
             /**
              * 比对key
              */
-            if (trimThemeKey.includes(trimCssKey)) {
+            if (themeItem.includes(cssItem.key)) {
               /**
                * 比对value
+               * 去掉空格&&大小写转换操作，为方便比较渐变属性、颜色等属性
                */
-              if (THEME_CSS_MAP[trimThemeKey].includes(trimCssVal)) {
-                cssItem.value = `var(${trimThemeKey})`;
+              const noSpaceThemeVal = THEME_CSS_MAP[themeItem]
+                .replaceAll(/\s/g, "")
+                .toLowerCase();
+              const noSpaceCssVal = cssItem.value
+                .replaceAll(/\s/g, "")
+                .toLowerCase();
+              if (noSpaceThemeVal.includes(noSpaceCssVal)) {
+                cssItem.value = `var(${themeItem})`;
               }
             }
           }
@@ -165,14 +185,17 @@ window.onload = function () {
 
     transCodeToCssArr = () => {
       const cssArr = this.codeBoxInnerHtml
-        .replace(/<\/?.+?>/g, "")
-        .replace(/\n/g, "")
+        .replaceAll(/<\/?.+?>/g, "")
+        .replaceAll(/\n/g, "")
         .split(";");
       if (cssArr.length) {
         return cssArr.map((code) => {
           if (code.length) {
             const codeArr = code.split(":");
-            const codeItem = { key: codeArr[0], value: codeArr[1].trim() };
+            const codeItem = {
+              key: codeArr[0],
+              value: codeArr[1]?.trim(),
+            };
             return codeItem;
           }
         });
@@ -192,7 +215,7 @@ window.onload = function () {
 
     transBtnDom = () => {
       const transBtn = this.createNode("span", "转换");
-      transBtn.setAttribute("style", this.styleStr);
+      transBtn.setAttribute("style", this.transBtnStyleStr);
       transBtn.setAttribute("id", "trans_code");
       transBtn.onclick = this.handleTransBtn;
       return transBtn;
@@ -200,7 +223,7 @@ window.onload = function () {
 
     resetBtnDom = () => {
       const resetBtn = this.createNode("span", "还原");
-      resetBtn.setAttribute("style", this.styleStr);
+      resetBtn.setAttribute("style", this.resetBtnStyleStr);
       resetBtn.setAttribute("id", "reset_code");
       resetBtn.onclick = this.handleResetBtn;
       return resetBtn;
@@ -238,6 +261,6 @@ window.onload = function () {
       this.monitorParentNode();
     };
   }
-  const instance = ChromePluginLanhuapp.getInstance();
+  const instance = ChromePluginLanhu.getInstance();
   instance.init();
 };
